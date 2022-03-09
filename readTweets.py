ea71bb6f4ad83cc931a49b3b4c4131a17a49a86e
Samuel Rivera <sriveravi@gmail.com>
2022-03-08 17:50:58 -0700

    now saving 2 tweet types

--- readTweets.py
+++ readTweets.py
@@ -11,25 +11,33 @@ auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
 
-friendName = "dog_feelings"
-friend = api.get_user(screen_name=friendName)
+user_list = [
+    {'name':'dog_feelings', 'file':'dog_tweets.txt'},
+    {'name':'feline_feelings', 'file':'cat_tweets.txt'}
+]
+
+for user in user_list:
+
+    friend = api.get_user(screen_name=user['name'])
+
+    # print("User details:")
+    # print(friend.name)
+    # print(friend.description)
+    # print(friend.location)
+
+    allTweets = []
+
+    numStatuses = 100
+    for status in Cursor(api.user_timeline, id=user['name']).items(numStatuses):
+        cleanStat = re.sub(r"http\S+", "", status.text)
+        
+        # print(cleanStat)
+        allTweets.append(cleanStat)
 
 
-# print("User details:")
-# print(friend.name)
-# print(friend.description)
-# print(friend.location)
 
-allTweets = []
-
-numStatuses = 100
-for status in Cursor(api.user_timeline, id=friendName).items(numStatuses):
-    cleanStat = re.sub(r"http\S+", "", status.text)
-    
-    print(cleanStat)
-    allTweets.append(cleanStat)
-
-
+    with open(user['file'], 'w') as f:
+        f.writelines(allTweets)
 
 # print( allTweets)
 
