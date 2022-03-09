78569f26be4baf1ed4bc2dc8ae66ea2a6e4ff5ba
Samuel Rivera <sriveravi@gmail.com>
2022-03-08 18:42:26 -0700

    getting 50% accuracy with a linear model

--- classify.py
+++ classify.py
@@ -2,6 +2,8 @@
 Extracting features and training a classifier
 """
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
 from transformers import BartTokenizer, BartModel
 
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
@@ -34,3 +36,19 @@ for class_idx, the_file in enumerate(file_list):
     features.append(np.vstack(data_list[class_idx]))
     labels.append(class_idx*np.ones((len(data_list[class_idx]),1)))
     names.append(the_file.split('.')[0])
+
+
+# do some classification
+X = np.vstack(features)
+y = np.vstack(labels)
+
+reg = LinearRegression().fit(X, y)
+score = reg.score(X, y, )
+
+# get some metrics
+print( f"R^2 score (1.0 is perfect, const is 0.): {score:.2e}")
+
+y_pred = reg.predict(X)
+y_pred = (y_pred>.5).astype(int)
+acc = accuracy_score(y, y_pred)
+print(f"Accuracy: {acc:.2f}")
