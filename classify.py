b7a96b9120eb185ae8dcf12e834b25da97731df3
Samuel Rivera <sriveravi@gmail.com>
2022-03-11 08:13:59 -0700

    fixed a weird bug where it was making both data lists bigger instead of each separately. I have no idea what the heck was going on there. I fixed by using a dict structure for each class separately.

--- classify.py
+++ classify.py
@@ -6,37 +6,41 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from transformers import BartTokenizer, BartModel
 from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
+from tqdm import tqdm
 
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 model = BartModel.from_pretrained('facebook/bart-base')
 
-file_list = ['cat_tweets.txt', 'dog_tweets.txt']
-data_list = [[]]*len(file_list)
 
+class_dict_list = [
+    {'file': 'cat_tweets.txt', 'data':[]},
+    {'file': 'dog_tweets.txt', 'data':[]}]
 
 # loop through files and extract features
-for class_idx, the_file in enumerate(file_list):
+for class_idx, class_obj in enumerate(class_dict_list):
 
-    with open(the_file, 'r') as f:
+    with open(class_obj['file'], 'r') as f:
         text_list = f.readlines()
 
-    for line in text_list:
+    for line in tqdm(text_list):
 
         inputs = tokenizer(line, return_tensors="pt", truncation=True)
         outputs = model(**inputs)
-
-        last_hidden_states = outputs.last_hidden_state
-        data_list[class_idx].append(last_hidden_states.detach().numpy().mean(axis=1))
+        last_hidden_states = outputs.last_hidden_state        
+        line_features = last_hidden_states.detach().numpy().mean(axis=1)
+                        
+        class_obj['data'].append(line_features)
 
 
 # make the class labels
 features = []
 labels = []
 names = []
-for class_idx, the_file in enumerate(file_list):
-    features.append(np.vstack(data_list[class_idx]))
-    labels.append(class_idx*np.ones((len(data_list[class_idx]),1)))
-    names.append(the_file.split('.')[0])
+for class_obj in enumerate(class_dict_list):
+    features.append(np.vstack(class_obj['data']))
+    labels.append(class_idx*np.ones((len(class_obj['data']),1)))
+    names.append(class_obj['file'].split('.')[0])
 
 
 # do some classification
@@ -55,3 +59,7 @@ y_pred = reg.predict(X)
 # y_pred = (y_pred>.5).astype(int)
 # acc = accuracy_score(y, y_pred)
 # print(f"Accuracy: {acc:.2f}")
+
+
+
+X_pca = PCA(n_components=2).fit_transform(X)
